5d4506bee75fc3ac5922978cd9c0c579195caa14
Hugo O. Rivera <hugo@roguh.com>
2021-10-31 00:07:10 -0600

    Fix bugs, print more in test-install.sh

--- test-install.sh
+++ test-install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-if command -v gp > /dev/null; then
+if ! command -v gp > /dev/null; then
     echo Command gp not found
     echo Check to make sure gp is in your PATH "$1"
+    exit 1
 fi
 
 if ! gp -h; then
     echo Command gp found, but failed to run
+    exit 1
 fi
+
+echo Succesfully installed!
